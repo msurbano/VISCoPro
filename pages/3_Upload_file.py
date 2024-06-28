50c3620a49b4f8f9decb4b08c432c7f0903ead6e
María Salas Urbano <msurbano@us.es>
2024-06-28 09:52:49 +0200

    Update 3_Upload_file.py

--- pages/3_Upload_file.py
+++ pages/3_Upload_file.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 from io import StringIO
 import pm4py
+help(pm4py.discovery.discover_dfg)
 
 # from typing import Tuple, Union, List, Dict, Any, Optional, Set
 

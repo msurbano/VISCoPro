9b0f6123c9d35096e7425d3a96a5350289d71568
María Salas Urbano <msurbano@us.es>
2024-10-16 12:10:22 +0200

    Update 1_Pattern_specification.py

--- pages/1_Pattern_specification.py
+++ pages/1_Pattern_specification.py
@@ -654,7 +654,7 @@ if len(st.session_state.dataframe):
 
     translater={"Absolute Frequency":"abs_freq","Case Frequency":"case_freq",
                     "Max Repetitions":"max_repetitions", "Total Repetitions":
-                    "total_repetitions","Median CT":"median CT","Mean CT":"mean CT","StDev CT":"stdev","Total CT":"sum"}
+                    "total_repetitions","Median CT":"median CT","Mean CT":"mean CT","StDev CT":"stdev","Total CT":"total CT"}
 
     measure=translater[metric]
 
